feec440e84c453ddedb956cc52205c6b306cdc9a
Alessio Frittoli <info@alessiofrittoli.it>
2024-12-15 22:44:45 +0100

    feature: add `Exception`class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,35 @@
-// 
+export interface ExceptionOptions<TCode = number> extends ErrorOptions
+{
+	code	: TCode
+	name?	: string
+	status?	: number
+}
+
+
+class Exception<TCode = number> extends Error implements ExceptionOptions<TCode>
+{
+	status
+	name
+	code
+
+
+	constructor( message: string, options: ExceptionOptions<TCode> ) 
+	{
+		super( message, options )
+
+		this.name		= options.name || 'Exception'
+		this.message	= message
+		this.code		= options.code
+		this.status		= options.status
+	}
+
+	
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	static isException<TCause = number>( error: any ): error is Exception<TCause>
+	{
+		return error instanceof Exception
+	}
+}
+
+
+export default Exception
