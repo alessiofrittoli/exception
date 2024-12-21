c1bf19bcbf436199c74809fae1d10e3165c449ec
Alessio Frittoli <35973186+alessiofrittoli@users.noreply.github.com>
2024-12-21 12:28:53 +0100

    feature/error-code (#6)
    * feature: add `ErrorCode` with most common error codes
    
    * chore: improved `Exception` types

---
+++ src/code.ts
@@ -0,0 +1,16 @@
+/**
+ * Most common error codes.
+ * 
+ */
+enum ErrorCode
+{
+	UNKNOWN		= 'ERR:UNKNOWN',
+	ABORT		= 'ERR:ABORT',
+	EMPTY_VALUE	= 'ERR:EMPTYVALUE',
+	WRONG_VALUE	= 'ERR:WRONGVALUE',
+	EXPIRED		= 'ERR:EXPIRED',
+	TOO_EARLY	= 'ERR:TOOEARLY',
+}
+
+
+export default ErrorCode
