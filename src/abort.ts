46a23a50340ea6f8418dc59aa72e496dea5a3e0d
Alessio Frittoli <info@alessiofrittoli.it>
2025-04-26 19:32:53 +0200

    feat: add `AbortError.isAbortError()` static method

--- src/abort.ts
+++ src/abort.ts
@@ -26,4 +26,21 @@ export class AbortError<TMessage> extends Exception<TMessage, ErrorCode.ABORT> i
 	{
 		super( reason, { name: 'AbortError', code: ErrorCode.ABORT, ...options } )
 	}
+
+
+	/**
+	 * Determines if the provided error is an instance of the AbortError class and has the ABORT ErrorCode.
+	 * 
+	 * This won't work for `new DOMException( 'Abort reason', 'AbortError' )` since we type guard the checked value to `AbortError<TMessage>`.
+	 *
+	 * @template TMessage The type of the message property of the AbortError.
+	 * 
+	 * @param	error The error to check.
+	 * @returns	`true` if the error is an `AbortError` and has the ABORT ErrorCode, `false` otherwise.
+	 */
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	static isAbortError<TMessage = string>( error: any ): error is AbortError<TMessage>
+	{
+		return super.isAbortError( error )
+	}
 }
