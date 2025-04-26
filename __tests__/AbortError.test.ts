0f35894efefba490d952f042de59549fdf6879d6
Alessio Frittoli <info@alessiofrittoli.it>
2025-04-26 19:06:40 +0200

    tests: add `AbortError` unit tests

---
+++ __tests__/AbortError.test.ts
@@ -0,0 +1,34 @@
+import { AbortError } from '@/abort'
+import { ErrorCode } from '@/code'
+
+describe( 'AbortError', () => {
+
+	it( 'creates an instance of AbortError with default options', () => {
+
+		const reason	= 'Operation aborted'
+		const error		= new AbortError( reason )
+
+		expect( error ).toBeInstanceOf( AbortError )
+		expect( error.name ).toBe( 'AbortError' )
+		expect( error.code ).toBe( ErrorCode.ABORT )
+		expect( error.message ).toBe( reason )
+		expect( error.status ).toBeUndefined()
+
+	} )
+
+
+	it( 'creates an instance of AbortError with custom options', () => {
+
+		const reason	= 'Operation aborted'
+		const options	= { status: 400 }
+		const error		= new AbortError( reason, options )
+
+		expect( error ).toBeInstanceOf( AbortError )
+		expect( error.name ).toBe( 'AbortError' )
+		expect( error.code ).toBe( ErrorCode.ABORT )
+		expect( error.message ).toBe( reason )
+		expect( error.status ).toBe( options.status )
+
+	} )
+
+} )
