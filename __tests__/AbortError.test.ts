0c0fd0a019147441da4d3348e94ca83608c6bd23
Alessio Frittoli <info@alessiofrittoli.it>
2025-04-29 19:44:55 +0200

    tests: add custom AbortError code unit tests

--- __tests__/AbortError.test.ts
+++ __tests__/AbortError.test.ts
@@ -1,6 +1,11 @@
 import { AbortError } from '@/abort'
 import { ErrorCode } from '@/code'
 
+enum CustomErrorCodes
+{
+	CUSTOM_CODE = 'ERR:CUSTOM_ABORT_CODE',
+}
+
 describe( 'AbortError', () => {
 
 	it( 'creates an instance of AbortError with default options', () => {
@@ -32,6 +37,15 @@ describe( 'AbortError', () => {
 	} )
 
 
+	it( 'allows a custom AbortError code', () => {
+
+		const error = new AbortError( 'Operation aborted', { code: CustomErrorCodes.CUSTOM_CODE } )
+
+		expect( error.code ).toBe( CustomErrorCodes.CUSTOM_CODE )
+
+	} )
+
+
 	describe( 'AbortError.isAbortError()', () => {
 	
 		it( 'checks if error is an AbortError with ABORT ErroCode', () => {
@@ -40,6 +54,18 @@ describe( 'AbortError', () => {
 				AbortError.isAbortError( new AbortError( 'Abort Reason' ) )
 			).toBe( true )
 
+		} )
+
+
+		it( 'checks if error is an AbortError with custom ABORT ErroCode', () => {
+			
+			expect(
+				AbortError.isAbortError(
+					new AbortError( 'Operation aborted', { code: CustomErrorCodes.CUSTOM_CODE } ),
+					CustomErrorCodes.CUSTOM_CODE,
+				)
+			).toBe( true )
+	
 		} )
 
 	} )
