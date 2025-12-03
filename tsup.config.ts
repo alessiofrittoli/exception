4c9c773e77e7dcf432d43c4e22edcf377ac327fa
Alessio Frittoli <35973186+alessiofrittoli@users.noreply.github.com>
2025-12-03 18:40:11 +0100

    bump: update project deps (#34)

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'tsup'
 
-const enableSourcemap = process.env.NODE_ENV !== 'production'
+const isProduction = process.env.NODE_ENV === 'production'
 
 export default defineConfig( {
 	entry		: [ 'src/**/*.ts' ],
@@ -10,5 +10,6 @@ export default defineConfig( {
 	shims		: true,
 	skipNodeModulesBundle: true,
 	clean		: true,
-	sourcemap	: enableSourcemap,
+	minify		: isProduction,
+	sourcemap	: ! isProduction,
 } )
